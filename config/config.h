cbd8b35bd12474db633ef67d7194f76195a4c21b
shubhindia <shubhindia123@gmail.com>
2025-01-18 02:44:09 +0530

    enable display
    Signed-off-by: shubhindia <shubhindia123@gmail.com>

---
+++ config/config.h
@@ -0,0 +1,3 @@
+#define ENCODERS_PAD_A {F5}   // Pro Micro pin on Out A
+#define ENCODERS_PAD_B {F4}   // Pro Micro pin on Out B
+#define ENCODER_RESOLUTIONS 4 // Pulses the encoder registers between each detent
